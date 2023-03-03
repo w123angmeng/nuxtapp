c47f9b2c7b926cf1717d13571f273f4fb2c41c4b
wang meng <wangm@gih.cn>
2023-03-03 18:21:59 +0800

    打包后路径

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@element-plus/nuxt"],
-//   app: {
-//     head: {
-//       charset: 'utf-8',
-//       viewport: 'width=500, initial-scale=1',
-//       title: '官网',
-//       meta: [
-//         // <meta name="description" content="My amazing site">
-//         { name: 'description', content: '这是一段文字描述' },
-//         { name: 'keywords', content: '这是一段关键字' }
-//       ],
-//     }
-//   }
+  app: {
+    baseURL:'./',
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=500, initial-scale=1',
+      title: '官网',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: '这是一段文字描述' },
+        { name: 'keywords', content: '这是一段关键字' }
+      ],
+    }
+  }
 });
